e7ff2e0bfb53bce4fccee66bcd992a7959152651
Your Name <you@example.com>
2022-01-24 21:11:46 -0500

    set up angular

---
+++ Angular/descargar_angular.js
@@ -0,0 +1,4 @@
+//npm install (-g) @angular/cli para instalar angular
+//ng new nombre_proyecto para establecer la carpeta del proyecto
+//ng serve (--port puerto)serve para arrancar la aplicacion en un servidor
+//url http://localhost:4200/
