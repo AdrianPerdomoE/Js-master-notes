aa961dea70aba8132a5bf254b0d7db161b4a1ece
Your Name <you@example.com>
2022-02-12 22:44:17 -0500

    diseño formulario crear proyecto

--- Proyecto_Angular/src/app/components/create/create.component.ts
+++ Proyecto_Angular/src/app/components/create/create.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project';//importar el modelo de proyecto
+import { ProjectService } from 'src/app/services/project.service';//importar los servicios
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css']
+  styleUrls: ['./create.component.css'],
+  providers:[ProjectService]
 })
 export class CreateComponent implements OnInit {
-
-  constructor() { }
+  public title:string;
+  public project:Project;
+  constructor(
+    private _projectService:ProjectService
+  ) {
+    this.title="Crear proyecto";
+    this.project=new Project("","","","",0,"","");
+   }
 
   ngOnInit(): void {
+  }
+  onSubmit(projectForm:any){
   }
 
 }
