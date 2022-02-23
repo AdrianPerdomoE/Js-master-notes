7b9faa68219148f9a3b6d47b45d4c0b02d0bec31
Your Name <you@example.com>
2022-02-22 22:31:32 -0500

    maquetado pagina de proyecto

--- Proyecto_Angular/src/app/components/detail/detail.component.ts
+++ Proyecto_Angular/src/app/components/detail/detail.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Project } from 'src/app/models/Project';
+import { ProjectService } from 'src/app/services/project.service';
+import { Global } from 'src/app/services/global';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
+  providers:[ProjectService]
 })
 export class DetailComponent implements OnInit {
-
-  constructor() { }
+  public url:string;
+  public project:Project;
+  constructor(
+    private _projectService:ProjectService,
+    private _router:Router,
+    private _route:ActivatedRoute 
+  ) {
+    this.url=Global.url;
+    this.project=new Project( "","","","",0,"","");
+   }
 
   ngOnInit(): void {
+    this._route.params.subscribe(params=>{
+      let id = params["id"];
+      this.getProject(id);
+    });
+  }
+  getProject(id:string){
+    this._projectService.getProject(id).subscribe(
+      Response=>{
+        this.project= Response.project;
+      }
+      );
   }
 
 }
