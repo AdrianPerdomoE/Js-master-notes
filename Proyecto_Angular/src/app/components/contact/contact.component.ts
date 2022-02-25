c0b780a36d52ff63b2496698f0a307d7ed5bca2c
Your Name <you@example.com>
2022-02-24 20:26:49 -0500

    Boton con animacion

--- Proyecto_Angular/src/app/components/contact/contact.component.ts
+++ Proyecto_Angular/src/app/components/contact/contact.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
-
+  animando = false;
   constructor() { }
 
   ngOnInit(): void {
